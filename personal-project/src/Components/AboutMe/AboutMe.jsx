9d431f5b7fbad94bf11fcc75bc099a93efd2a419
unknown <nath2498@yahoo.co.uk>
2024-04-04 12:33:48 +0100

    Personal profile update

--- personal-project/src/Components/AboutMe/AboutMe.jsx
+++ personal-project/src/Components/AboutMe/AboutMe.jsx
@@ -1,13 +1,35 @@
 function AboutMe() {
     return (
         <div>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <h1>"This is a my about me apparently”</h1>
-        </div>
+            <br />
+            <br />
+            <h1>Professional Profile</h1>
+            <p>
+                Currently undertaking a reskilling/ bootcamp oppurtunity to learn how to operate as a full stack engineer.
+                Built foundation of knowledge across front and back end development. Started my journey by learning a variety
+                of GitHub, Git Bash, CSS, HTML, Javascript, ReactJs, Java, Springboot, SQL alongside other features and applications.
+            </p>
+            <h1>Career Ambitions</h1>
+            <p>
+                Short term my plan is to absorb and develop my knowledgeas much as possible whilst still undertaking my
+                reskilling oppurtunity. Within the near future I would be looking to secure a tech based role where I can
+                use my learning and actively implement this into a working environment. Being able to make a positive contirbution
+                and learning the workplace infrastructure.
+                <br />
+                <br />
+                Long term I am looking to become profficient in a range of different skills and applications used within software
+                engineering. I would want to be pushing myself to becoming a mentor or team lead to be able to support other engineers
+                and to be able to provide a more positive impact within a workplace.
+            </p>
+<br/>
+<br/>
+<br/>
+<br/>
+<h1>Personal Profile</h1>
+// Make 4 cards - Pictures of rugby, walking, daily/ social and work. Give images links e.g. Dinas website for rugby. brief 
+overview underneath. Maybe have a button to have some sort of feature.
+
+</div>
     );
 }
 
